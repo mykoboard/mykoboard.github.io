dc7e313aa0d5fd83820f2d59157d63d49752e909
dParadiz <dparadiz@gmail.com>
2026-03-07 17:40:04 +0100

    refactor: extract apex-nebula into standalone repository

--- packages/apex-nebula/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import './index.css';
-import StandaloneApexNebula from './StandaloneApexNebula.vue';
-
-const app = createApp(StandaloneApexNebula);
-app.mount('#root');
